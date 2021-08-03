16e384fc44462d98f137c626309bd1d04b0e2f3f
Vivek Singh <petmongrels@gmail.com>
2021-08-03 18:37:53 +0530

    backup management

---
+++ Makefile
@@ -0,0 +1,16 @@
+postgres_user := $(shell id -un)
+
+tunnel-mysql-vagrant:
+	ssh -p 2222 -i ~/.vagrant.d/insecure_private_key vagrant@127.0.0.1 -L 6032:localhost:3306
+
+restore-backup-from-vagrant:
+	scp -P 2222 -i ~/.vagrant.d/insecure_private_key root@127.0.0.1:/root/source/abi-host/backup/backup.sql /tmp/abi-backup.sql
+
+restore-remote-backup:
+	sh scripts/restore-db.sh backup/remote-backup.sql $(postgres_user)
+
+backup-db:
+	sh scripts/backup-db.sh
+
+copy-backup-on-vagrant:
+	scp -P 2222 -i ~/.vagrant.d/insecure_private_key backup/backup.sql root@127.0.0.1:/root/source/abi-host/backup/remote-backup.sql
--- scripts/restore-db.sh
+++ scripts/restore-db.sh
@@ -1,5 +1,5 @@
-sudo -u postgres psql postgres -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = 'bahmni_avni' AND pid <> pg_backend_pid()"
-sudo -u postgres psql postgres -c 'drop database bahmni_avni'
-sudo -u postgres psql postgres -c "create user bahmni_avni_admin with password 'password'"
-sudo -u postgres psql postgres -c 'create database bahmni_avni with owner bahmni_avni_admin'
-sudo -u postgres psql bahmni_avni -f $1
+sudo -u $2 psql postgres -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = 'bahmni_avni' AND pid <> pg_backend_pid()"
+sudo -u $2 psql postgres -c 'drop database bahmni_avni'
+sudo -u $2 psql postgres -c "create user bahmni_avni_admin with password 'password'"
+sudo -u $2 psql postgres -c 'create database bahmni_avni with owner bahmni_avni_admin'
+sudo -u $2 psql bahmni_avni -f $1
